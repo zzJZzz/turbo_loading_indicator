80dfd36cb5c00a8b1ba0b79c36ac01978d4bada4
zzJZzz <zzjzzz.creator@gmail.com>
2023-09-07 14:50:34 -0400

    Init Commit

---
+++ lib/turbo_loading_indicator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "turbo_loading_indicator/version"
+
+module TurboLoadingIndicator
+  class Error < StandardError; end
+  # Your code goes here...
+end
